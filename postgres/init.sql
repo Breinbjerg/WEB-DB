509d7f9edd2fe9e36578f35d2937e82f74d178e7
Steffen Breinbjerg <steffenpb@outlook.dk>
2019-12-04 17:24:07 +0100

    Added Files

---
+++ postgres/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE public.persons (
+    firstName varchar(255),
+    lastName varchar(255)
+);
+
+INSERT INTO persons(firstName, lastName)
+VALUES ('Steffen','Breinbjerg'),('Nanna','Overgaard'),('Jan','Nielsen'),('Tina','Nielsen'),
+('Nikolaj','Overgaard'),('Berit','Præstholm'),('Kurt','Breinbjerg');
+
+
